647c4cf126205fdc8e6bc921036eaed25ca434fc
Lalitha <lalitha.d339@gmail.com>
2023-08-05 16:44:22 +0530

    terraform instance

--- roboshop-v1/roboshop.tf
+++ roboshop-v1/roboshop.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
-
+  vpc_security_group_ids = [ "sg-0ae5a9e3d447a06e1" ]
   tags = {
     Name = "frontend"
   }
@@ -10,7 +10,7 @@ resource "aws_instance" "frontend" {
 resource "aws_instance" "mongodb" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
-
+ vpc_security_group_ids = [ "sg-0ae5a9e3d447a06e1" ]
   tags = {
     Name = "mongodb"
   }
@@ -18,7 +18,7 @@ resource "aws_instance" "mongodb" {
 resource "aws_instance" "catalogue"{
  ami            ="ami-03265a0778a880afb"
  instance_type  ="t3.micro"
-
+ vpc_security_group_ids = [ "sg-0ae5a9e3d447a06e1" ]
   tags   = {
     Name ="catalogue"
   }
