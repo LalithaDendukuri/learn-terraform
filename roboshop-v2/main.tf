c06cb190993d981f7dd37f0ab4ef1dbfb8f5412f
Lalitha <lalitha.d339@gmail.com>
2023-08-05 18:53:13 +0530

    terraform variables

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -1,15 +1,45 @@
-variable "components" {
- default = ["frontend","mongodb"]
+variable "ami"{
+    default= "ami-03265a0778a880afb"
+}
+variable "security_group"{
+    default = [ "sg-0ae5a9e3d447a06e1" ]
+}
+variable "instance_type" {
+    default = "t3.micro"
+}
+variable "zone_id" {
+    default = "Z0893432XKXNV5K4OIQB"
+}
+resource "aws_instance" "frontend" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids =var.security_group
+  tags = {
+    Name = "frontend"
+  }
+}
+resource "aws_route53_record" "frontend" {
+  zone_id = var.zone_id
+  name    = "frontend-dev.dljrobo.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
 }
 
-variable "components" {
- default = {
-    frontend {
-     instance_type="t3.small"
-    }
- mongodb {
-     instance_type="t3.small"
-    }
+resource "aws_instance" "mongodb" {
+   ami           = var.ami
+    instance_type = var.instance_type
+    vpc_security_group_ids =var.security_group
+  tags = {
+    Name = "mongodb"
+  }
+}
+resource "aws_instance" "catalogue"{
+  ami           = var.ami
+   instance_type = var.instance_type
+   vpc_security_group_ids =var.security_group
+  tags   = {
+    Name ="catalogue"
+  }
 
- }
 }
