9bc6e2b48d59867d814e62c3c9952e5c78e11ca9
Lalitha <lalitha.d339@gmail.com>
2023-08-05 17:38:04 +0530

    terraform variables

--- vars/main.tf
+++ vars/main.tf
@@ -1,9 +1,48 @@
+//Palin variable
 variable "fruit_name"{
     default = "apple"
 }
 
-output "fruit"{
+output "fruit_name"{
     //value = var.fruit_name.value
 
      value = var.fruit_name
+}
+// list variable
+variable "fruitList"{
+    default = [ "apple" ,"orange"]
+
+}
+output "fruitList_first"{
+       value = var.fruitList[0]
+}
+
+output "fruitList_second"{
+       value = var.fruitList[1]
+}
+// Map variable plain
+variable "fruitMap"{
+    default = { apple=100 banana=200  }
+}
+output "fruitMap_apple"{
+       value = var.fruitMap["apple"]
+}
+
+output "fruitMap_banana"{
+       value = var.fruitMap["banana"]
+}
+
+// Map variable map of map
+variable "fruitMap_Maps"{
+    default = {
+    apple {
+     stock=100
+     price =1
+    }
+    banana{
+     stock=200
+     price=2}
+}
+output "fruitMap_Maps"{
+     //  value = var.fruitMap_Maps.i
 }
