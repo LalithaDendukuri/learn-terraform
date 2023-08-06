ff39216b748db5c7b4e3fb503d32c8dfb16b76a2
Lalitha <lalitha.d339@gmail.com>
2023-08-06 23:35:26 +0530

    terraform variables

--- roboshop-v3/var.tf
+++ roboshop-v3/var.tf
@@ -10,54 +10,48 @@ variable "components" {
     default = {
         frontend = {
         name = "frontend"
-        instance_type = "t3.micro
-        
-        "
+        instance_type = "t3.micro"
         }
-        mongodb = { name="mongodb"
-        instance_type = "t3.micro
-        
-        "
+        mongodb = {
+        name="mongodb"
+        instance_type = "t3.micro"
         }
-        catalogue = { name="catalogue"
-        instance_type = "t3.micro
-        
-        "
+        catalogue = {
+        name="catalogue"
+        instance_type = "t3.micro"
+
          }
-        reddis = { name ="reddis"
-        instance_type = "t3.micro
-        
-        "
+        reddis = {
+         name ="reddis"
+        instance_type = "t3.micro"
          }
-        cart = { name="cart"
-        instance_type = "t3.micro
-        
-        "
+        cart = {
+        name="cart"
+        instance_type = "t3.micro"
          }
-        user = { name="user"
-        instance_type = "t3.micro
-        
-        "
+        user = {
+        name="user"
+        instance_type = "t3.micro"
          }
-        mysql = { name="mysql"
-        instance_type = "t3.micro
-        
-        "
+        mysql = {
+         name="mysql"
+        instance_type = "t3.micro"
+
         }
-        shippment = { name="shippment"
-        instance_type = "t3.micro
-        
-        "
+        shippment = {
+         name="shippment"
+        instance_type = "t3.micro"
+
          }
-        payment = { name="payment"
-        instance_type = "t3.micro
-        
-        "
+        payment = {
+        name="payment"
+        instance_type = "t3.micro"
+
          }
-        dispatch = { name="dispatch"
-        instance_type = "t3.micro
-        
-        "
+        dispatch = {
+        name="dispatch"
+        instance_type = "t3.micro"
+
         }
 
 
