989729c1e76fd408667d142be38c209dd4c05fc2
Lalitha <lalitha.d339@gmail.com>
2023-08-06 23:28:30 +0530

    terraform variables

--- roboshop-v3/main.tf
+++ roboshop-v3/main.tf
@@ -1,10 +1,10 @@
 module "components"{
     for_each = var.components
 
-    source ="./module"
-    instance_type = each.value["instance_type"]
-    security_group = var.security_group
-    name =each.value["name"]
-    zone_id = var.zone_id
+    source          ="./module"
+    zone_id         = var.zone_id
+    security_group  = var.security_group
+    name            = each.value["name"]
+    instance_type   = each.value["instance_type"]
 }
 
